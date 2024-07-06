c5f68521376e3ac63fec12d75e343ea3e246bb30
m0destly <e1121889@u.nus.edu>
2024-07-07 05:07:00 +0800

    transaction by phone

--- app/screens/TransactionScreen.tsx
+++ app/screens/TransactionScreen.tsx
@@ -5,34 +5,69 @@ import axios from 'axios';
 
 const TransactionScreen = ({navigation} : any) => {
     const { userID } = useAppContext();
-    const [username, setUsername] = useState('')
+    const [username, setUsername] = useState('');
+    const [phone, setPhone] = useState('');
     const [targetUser, setTargetUser] = useState('');
     const [targetID, setTargetID] = useState('');
     const [amount, setAmount] = useState('');
+    const [isPhone, setIsPhone] = useState(true);
 
-    const checkRecipient = async () => {
+    const changeMode = () => {
+        setIsPhone(!isPhone);
+        setUsername('');
+        setPhone('');
+        setTargetUser('');
+        setTargetID('');
+    }
+
+    const checkRecipientUser = async () => {
         try {
-          const response = await fetch(`http://localhost:3000/getRecipient?user=${username}`);
-          const result = await response.json();
-    
-          if (response.status === 200) {
-            setTargetUser(result.name);
-            setTargetID(result.id);
-          } else if (!username){
-            setTargetUser('');
-            setTargetID('');
-          } else {
-            setTargetUser('User not found');
-            setTargetID('');
-          }
+            const response = await fetch(`http://localhost:3000/getRecipientUser?user=${username}`);
+            const result = await response.json();
+            
+            if (response.status === 200) {
+                setTargetUser(result.name);
+                setTargetID(result.id);
+            } else if (!username){
+                setTargetUser('');
+                setTargetID('');
+            } else {
+                setTargetUser('User not found');
+                setTargetID('');
+            }
         } catch (error) {
           console.error(error);
         }
-      };
+    };
+
+    const checkRecipientPhone = async () => {
+        try {
+
+            const response = await fetch(`http://localhost:3000/getRecipientPhone?phone=${encodeURIComponent(phone)}`);
+            const result = await response.json();
+            
+            if (response.status === 200) {
+              setTargetUser(result.name);
+              setTargetID(result.id);
+            } else if (!phone){
+              setTargetUser('');
+              setTargetID('');
+            } else {
+              setTargetUser('User not found');
+              setTargetID('');
+            }
+          } catch (error) {
+            console.error(error);
+          }
+    };
 
     const handleTransfer = async () => {
-        if (!targetUser|| !amount) {
-            Alert.alert('Error', 'Please fill all fields');
+        if (!targetID|| !amount) {
+            Alert.alert('Error', 'Please fill in all fields');
+            return;
+        }
+        if (userID === targetID) {
+            Alert.alert('Error', 'You cannot make transactions to yourself');
             return;
         }
 
@@ -54,11 +89,11 @@ const TransactionScreen = ({navigation} : any) => {
             <Text style={styles.title}>Transfer to Friends</Text>
             <TextInput
                 style={styles.input}
-                placeholder="Username"
-                keyboardType="numeric"
-                value={username}
-                onChangeText={setUsername}
-                onEndEditing={checkRecipient}
+                placeholder={isPhone ? "Phone" : "Username"}
+                keyboardType={isPhone ? "phone-pad" : "default"}
+                value={isPhone ? phone : username}
+                onChangeText={isPhone ? setPhone : setUsername}
+                onEndEditing={isPhone ? checkRecipientPhone : checkRecipientUser}
             />
             <TextInput
                 style={styles.input}
@@ -77,6 +112,7 @@ const TransactionScreen = ({navigation} : any) => {
                     <Text> You are transferring {amount} to {targetUser} </Text>
                 </>
             )}
+            <Button title={isPhone ? "By Username" : "By Phone"} onPress={changeMode}/>
             <Button title="Transfer" onPress={handleTransfer} />
         </View>
     );
